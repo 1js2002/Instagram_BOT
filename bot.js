84bc420c401bce1c057e88f1c8b93f7fa1a33f8c
1js2002 <dgoud99120@gmail.com>
2023-04-02 14:42:34 +0530

    wrote a loop to delete all the requests

--- bot.js
+++ bot.js
@@ -8,19 +8,20 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-(async function example() {
+(async function StartBot() {
   let driver = await new Builder().forBrowser('firefox').build();
   console.log('opening the browser');
   try {
     //go to instagram and login - done
     await driver.get('https://www.instagram.com');
     console.log('instagram is loaded');
+    await sleep(5000);
     await driver.wait(until.elementLocated(By.name('username')), 10000); // Wait for the username field to be located
     await driver.findElement(By.name('username')).sendKeys(process.env.INSTAGRAM_USERNAME); // Enter the username
     await driver.findElement(By.name('password')).sendKeys(process.env.INSTAGRAM_PASSWORD); // Enter the password and submit the form
-    console.log('logged into your acc');
     await sleep(3000);
     await driver.findElement(By.className('_acan _acap _acas _aj1-')).click(); // Click the login button
+    console.log('logged into your acc');
     
     // Wait for 5 seconds
     await sleep(5000);
@@ -44,16 +45,53 @@ function sleep(ms) {
     await driver.findElement(By.css('[aria-label=""]')).click();
     console.log('noti sidebar is loaded')
 
-    //delete the request
-    await sleep(5000);
+    //To delete 1 request
+    
+   /* await sleep(5000);
     await driver.wait(until.elementLocated(By.xpath('/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[2]/div[3]/div[2]/div')), 10000);
     await driver.wait(until.elementIsEnabled(driver.findElement(By.xpath('/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[2]/div[3]/div[2]/div'))), 10000); // Wait for the notifications to be clickable
     await driver.findElement(By.xpath('/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[2]/div[3]/div[2]/div')).click();
     console.log("deleted the request");
-    await sleep(10000);
+    await sleep(10000); */
 
-    await driver.wait(until.titleIs('Instagram'), 10000000); // Wait for the page title to change to "Instagram"
-  } finally {
-    //await driver.quit();
+    // loop to delete all requests
+    let count = 2;
+    while (true) {
+      try {
+        let xpath = `/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[${count}]/div[3]/div[2]/div`;
+        await driver.wait(until.elementLocated(By.xpath(xpath)), 10000);
+        let deleteElement = await driver.findElement(By.xpath(xpath));
+        await driver.executeScript("arguments[0].scrollIntoView(true);", deleteElement);
+        await driver.wait(until.elementIsEnabled(deleteElement), 10000);
+        await deleteElement.click();
+        console.log(`Deleted request ${count}`);
+        await sleep(3000);
+        count++;
+      } catch (err) {
+        console.log("No more requests to delete");
+        break;
+      }
+    }
+    //accepting requests
+    let Acount = 3;
+      while (true) {
+      try {
+        let xpath = `/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[${Acount}]/div[3]/div[1]/div`;
+        await driver.wait(until.elementLocated(By.xpath(xpath)), 10000);
+        let acceptElement = await driver.findElement(By.xpath(xpath));
+        await driver.executeScript("arguments[0].scrollIntoView(true);", acceptElement);
+        await driver.wait(until.elementIsEnabled(acceptElement), 10000);
+        await acceptElement.click();
+        console.log(`Accepted request ${Acount-2}`);
+        await sleep(3000);
+        Acount++;
+      } catch (err) {
+        console.log("No more requests to accept");
+        break;
+      }
+    }
+
+  } finally {    
+    await driver.quit();
   }
 })();
